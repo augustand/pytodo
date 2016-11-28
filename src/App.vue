b580ce799579a5860af1484bd3779191b43be5a8
baiyunhui <baiyunhui@juxinli.com>
2016-11-28 15:17:05 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <TodoList></TodoList>
+  </div>
+</template>
+
+<script>
+  import TodoList from './components/TodoList'
+
+  export default {
+    name: 'app',
+    components: {
+      TodoList
+    }
+  }
+</script>
---
+++ src/vuex/plugins.js
@@ -0,0 +1,13 @@
+import {STORAGE_KEY} from './store'
+import createLogger from '../../src/plugins/logger'
+
+var localStorage = window.localStorage
+const localStoragePlugin = store => {
+  store.subscribe((mutation, {todos}) => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  })
+}
+
+export default process.env.NODE_ENV !== 'production'
+  ? [createLogger(), localStoragePlugin]
+  : [localStoragePlugin]
